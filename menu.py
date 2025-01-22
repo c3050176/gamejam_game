0cf5d188b4588f2d99f2ed11b6ca753326e18e48
emily <emilygayagoy@gmail.com>
2025-01-22 13:52:40 +0000

    working play, quit and back buttons

--- menu.py
+++ menu.py
@@ -7,8 +7,8 @@ import button
 pygame.init()
 
 #create game window
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 800
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Main Menu")
@@ -24,20 +24,21 @@ font = pygame.font.SysFont("arialblack", 40)
 TEXT_COL = (255, 255, 255)
 
 #load button images
-play_img = pygame.image.load("images/play_img.png").convert_alpha()
+play_img = pygame.image.load("button_images/play_img.png").convert_alpha()
 #options_img = pygame.image.load("images/button_options.png").convert_alpha()
-quit_img = pygame.image.load("images/quit_img.png").convert_alpha()
+quit_img = pygame.image.load("button_images/quit_img.png").convert_alpha()
+back_img = pygame.image.load('button_images/back_img.png').convert_alpha()
 '''
 video_img = pygame.image.load('button_images/button_video.png').convert_alpha()
 audio_img = pygame.image.load('button_images/button_audio.png').convert_alpha()
 keys_img = pygame.image.load('button_images/button_keys.png').convert_alpha()
-back_img = pygame.image.load('button_images/button_back.png').convert_alpha()
 '''
 
 #create button instances
-play_button = button.Button(304, 125, play_img, 1)
+play_button = button.Button(226, 300, play_img, 0.5)
 #options_button = button.Button(297, 250, options_img, 1)
-quit_button = button.Button(336, 375, quit_img, 1)
+quit_button = button.Button(226, 450, quit_img, 0.5)
+back_button = button.Button(500, 450, back_img, 0.5)
 '''
 video_button = button.Button(226, 75, video_img, 1)
 audio_button = button.Button(225, 200, audio_img, 1)
@@ -85,10 +86,17 @@ while run:
 
   if not game:
     if menu_state == "main":
+      draw_text("best game ever", font, TEXT_COL, 100, 100)
       if play_button.draw(screen):
         game = True
       if quit_button.draw(screen):
         run = False
+  if game:
+    draw_text("insert game playing", font, TEXT_COL, 100,100)
+    if back_button.draw(screen):
+      game = False
+  menu_state = "main"
+
 
   #event handler
   for event in pygame.event.get():
