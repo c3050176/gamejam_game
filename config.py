1910276b72f9933af698de2dd193885fa0f45d9f
c3050176 <c3050176@newcastle.ac.uk>
2025-01-22 14:27:23 +0000

    Added basic classes for Person and Package.

---
+++ config.py
@@ -0,0 +1,16 @@
+
+
+class Person:
+    def __init__(self):
+        self.name = None
+        self.sex = None
+        self.house = None
+        self.tolerance = None # Use this to determine the character's demeanor toward the player
+
+class Package:
+    def __init(self):
+        self.recipient = None
+        self.value = None
+        self.size = None
+        self.weight = None
+        self.name = None
